4733a4c8136cf4a9ac8bc5a4407e9743fb35e900
Nick Hoszko <nhoszko@gmail.com>
2020-05-14 15:30:11 -0700

    still pfting around with images, objects in Image

---
+++ src/artwork/images.js
@@ -0,0 +1,13 @@
+const images = [
+  {id: 1, src: './tls.png', title: 'The Last Supperr'},
+  {id:2, src:'./annunciation.jpeg' , title: 'Annunciationr'},
+
+];
+
+const strings = [
+  {title: 'The Last Supper', year: 'c.1498', medium: 'tempura, gesso'},
+  {title: 'Annunciation', year: '1475-1480', medium: 'Oil and tempera on panel'},
+  {title: 'Saint John the Bapist', year: '1500', medium: 'Oil on walnut wood'}
+];
+
+export default images;
