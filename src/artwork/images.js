75c54d205950bcc3efec7f11fee9bf3e2f197215
Nick Hoszko <nhoszko@gmail.com>
2020-05-14 23:14:42 -0700

    larage array

--- src/artwork/images.js
+++ src/artwork/images.js
@@ -1,7 +1,17 @@
 const images = [
-  {id: 1, src: '/artwork/tls.png', title: 'The Last Supperr'},
-  {id:2, src:'/artwork/annunciation.jpeg' , title: 'Annunciationr'},
-
+  {id: 1, src: '/artwork/salvatorMundi.jpg', title: 'Salvator Mundi'},
+  {id:2, src:'/artwork/annunsdfsadf.jpg' , title: 'Annunciationr'},
+  {id:3, src:'/artwork/stJohnBaptist.jpg', title: 'Saint John the Baptist'},
+  {id:4, src:'/artwork/adorationOfMagi.jpg', title:'Adoration of the Magi'},
+  {id:5, src:'/artwork/bacchus.jpg', title:'Bacchus'},
+  {id:6, src:'/artwork/baptismOfChrist.jpg', title:'The Baptism Of Christ'},
+  {id:7, src:'/artwork/ginevraDeBenci.jpg', title:'Ginevra de\'Benci'},
+  {id:8, src:'/artwork/laBelleFerronniere.jpg', title:'La belle ferronniere'},
+  {id:9, src:'/artwork/ladyWithErmine.jpg', title:'Lady With An Ermine'},
+  {id:10, src:'/artwork/monaLisa.jpg', title:'Mona Lisa'},
+  {id:11, src:'/artwork/portraitOfManRedChalk.jpg', title:'Portrait of a Man in Red Chalk'},
+  {id:12, src:'/artwork/stJeromeWilderness.jpg', title:'Saint Jerome in the Wilderness'},
+  {id:13, src:'/artwork/studyHand.jpg', title:'Study of Hand'}
 ];
 
 const strings = [
--- src/components/Image.js
+++ src/components/Image.js
@@ -5,13 +5,13 @@ const Image = () => (
   <div>
     {/* THIS IS THE OBJECTS  */}
     {images.map(({id, src, title}) => <img key={id} src={src} title={title} />)}
-    
+
   {/* <img src={require(tls)} /> */}
   {/* <p>{strings[0].title}</p> */}
 
   {/* {images.map(({title, year, medium}) => <p>{title} was created {year} by a medium of {medium}.</p>)} */}
   
-  {images.map(source =>  <img src={source.src} />)}
+  {/* {images.map(source =>  <img src={source.src} />)} */}
 
   </div>
 );
