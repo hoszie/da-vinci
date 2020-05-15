7cb54aa6db9ed3187de16488b989361e4a3e9f42
Nick Hoszko <nhoszko@gmail.com>
2020-05-14 22:26:32 -0700

    rendered images-moved files into public/artwork

--- src/artwork/images.js
+++ src/artwork/images.js
@@ -1,6 +1,6 @@
 const images = [
-  {id: 1, src: './tls.png', title: 'The Last Supperr'},
-  {id:2, src:'./annunciation.jpeg' , title: 'Annunciationr'},
+  {id: 1, src: '/artwork/tls.png', title: 'The Last Supperr'},
+  {id:2, src:'/artwork/annunciation.jpeg' , title: 'Annunciationr'},
 
 ];
 
@@ -8,6 +8,6 @@ const strings = [
   {title: 'The Last Supper', year: 'c.1498', medium: 'tempura, gesso'},
   {title: 'Annunciation', year: '1475-1480', medium: 'Oil and tempera on panel'},
   {title: 'Saint John the Bapist', year: '1500', medium: 'Oil on walnut wood'}
-];
+]; 
 
 export default images;
--- src/components/Image.js
+++ src/components/Image.js
@@ -3,16 +3,15 @@ import images from '../artwork/images';
 
 const Image = () => (
   <div>
+    {/* THIS IS THE OBJECTS  */}
     {images.map(({id, src, title}) => <img key={id} src={src} title={title} />)}
+    
   {/* <img src={require(tls)} /> */}
   {/* <p>{strings[0].title}</p> */}
 
-  {images.map(({title, year, medium}) => <p>{title} was created {year} by a medium of {medium}.</p>)}
+  {/* {images.map(({title, year, medium}) => <p>{title} was created {year} by a medium of {medium}.</p>)} */}
   
   {images.map(source =>  <img src={source.src} />)}
-  
-  
-  <img src='https://en.wikipedia.org/wiki/The_Last_Supper_(Leonardo)#/media/File:The_Last_Supper_-_Leonardo_Da_Vinci_-_High_Resolution_32x16.jpg' />
 
   </div>
 );
